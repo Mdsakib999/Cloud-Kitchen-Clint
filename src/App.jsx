8c80575b7453dd7d65fd76884d2e93a83924b715
LaZyFee <rhr277@gmail.com>
2025-06-17 17:01:38 +0600

    About & Contact page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
-import "./App.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/SharedComponent/Navbar";
 import Footer from "./Components/SharedComponent/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="">
+    <div className="bg-bg-primary min-h-screen">
       {/* Nav bar */}
-      <Navbar></Navbar>
+      <Navbar />
 
       {/* Children component */}
       <div className="min-h-[calc(100vh-196px)] ">
@@ -18,7 +14,7 @@ function App() {
       </div>
 
       {/* Footer */}
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 }
