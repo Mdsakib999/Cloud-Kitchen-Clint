6060bad0c0439f1c6354d04a4338dd9a754c062a
sadikatanisha <sadikatanisha@gmail.com>
2025-06-18 16:56:45 +0600

    Add: redux setup

---
+++ src/redux/apiSlice.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000/api",
+    credentials: "include",
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem("accessToken");
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+
+  endpoints: (builder) => ({}),
+});
+
+export const {} = apiSlice;
