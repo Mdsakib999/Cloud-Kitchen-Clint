760629efe19bfe028649b6143737f9a230c0413d
LaZyFee <rhr277@gmail.com>
2025-06-28 18:59:10 +0600

    create blog page created

--- src/routs.jsx
+++ src/routs.jsx
@@ -36,6 +36,8 @@ import AddOffer from "./Components/AdminDashBoardPageComponent/Offer/AddOffer";
 import AddFood from "./Components/AdminDashBoardPageComponent/AddFood/AddFood";
 import Faq from "./Components/Faq";
 import ManageFood from "./Components/AdminDashBoardPageComponent/ManageFood/ManageFood";
+import { CreateBlog } from "./Components/AdminDashBoardPageComponent/Blogs/CreateBlog";
+import { ManageBlog } from "./Components/AdminDashBoardPageComponent/Blogs/ManageBlog";
 
 export const router = createBrowserRouter([
   {
@@ -193,6 +195,14 @@ export const router = createBrowserRouter([
       {
         path: "manage-coupon",
         element: <ManageCoupon />,
+      },
+      {
+        path: "create-blogs",
+        element: <CreateBlog />,
+      },
+      {
+        path: "manage-blogs",
+        element: <ManageBlog />,
       },
     ],
   },
