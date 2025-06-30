d74d3b8e6c5b8c9855821270ae02914be1852178
Md. Shahidul Islam <shanto57575@gmail.com>
2025-06-30 16:21:28 +0600

    order history running

--- src/Components/AdminDashBoardPageComponent/OrderPage/OrderDetails.jsx
+++ src/Components/AdminDashBoardPageComponent/OrderPage/OrderDetails.jsx
@@ -1,19 +1,18 @@
-import { useParams } from "react-router-dom";
-import { orders } from "../../../FakeDB/mockOrders";
+import { useLocation } from "react-router-dom";
 import { CustomerInfoCard } from "./CustomerInfoCard";
-import { OrderNoteCard } from "./OrderNoteCard";
 import { OrderItemsTable } from "./OrderItemsTable";
 import { useState } from "react";
 import { OrderHistoryTimeline } from "./OrderHistoryTimeline";
 import DeliveryTracker from "./DeliveryTracker";
 
 export const OrderDetails = () => {
-  const { id } = useParams();
-  const order = orders.find((o) => o.oid === id);
   const [orderStatus, setOrderStatus] = useState("pending");
+  const location = useLocation();
+  const order = location.state || {};
+  console.log(order);
 
   if (!order) {
-    return <div>Order not found</div>;
+    return <div className="text-xl min-h-screen">Order not found</div>;
   }
 
   return (
@@ -21,13 +20,14 @@ export const OrderDetails = () => {
       {/* Header */}
       <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4 mb-6">
         <h1 className="text-xl sm:text-2xl font-bold text-gray-800">
-          Order ID: #{order.oid}
+          Order ID: #OID{order._id.slice(-4)}
         </h1>
 
         <div className="flex gap-2">
-          <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition-colors text-sm">
+          {/* Order cancel option */}
+          {/* <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition-colors text-sm">
             Cancel Order
-          </button>
+          </button> */}
 
           <select
             value={orderStatus}
@@ -36,9 +36,10 @@ export const OrderDetails = () => {
           >
             <option value="pending">Pending</option>
             <option value="accepted">Accepted</option>
-            <option value="rejected">Rejected</option>
-            <option value="processing">Processing</option>
-            <option value="on-delivery">On Delivery</option>
+            <option value="cancelled">Cancelled</option>
+            <option value="preparing">Preparing</option>
+            <option value="ready">Ready</option>
+            <option value="delivering">Delivering</option>
             <option value="delivered">Delivered</option>
           </select>
         </div>
@@ -48,19 +49,31 @@ export const OrderDetails = () => {
       <div className="flex flex-col lg:flex-row gap-6 items-center justify-center">
         {/* Left: Customer Info & Note */}
         <div className="w-full lg:w-1/4 flex flex-col gap-4">
-          <CustomerInfoCard name={order.name} />
-          <OrderNoteCard note={order.note} address={order.address} />
+          <CustomerInfoCard
+            name={order?.name}
+            email={order?.email}
+            note={order?.additionalInfo}
+            phone={order?.phone}
+            address={order?.address}
+          />
         </div>
 
         {/* Right: Order Items */}
         <div className="w-full lg:w-3/4">
-          <OrderItemsTable items={order.items} />
+          <OrderItemsTable
+            items={order?.items}
+            discountPrice={order?.discountPrice}
+          />
         </div>
       </div>
 
       {/* Timeline & Delivery */}
       <div className="mt-10 grid grid-cols-1 lg:grid-cols-2">
-        <OrderHistoryTimeline />
+        <OrderHistoryTimeline
+          orderTime={order?.createdAt}
+          status={order?.order_status}
+          isPaid={order?.isPaid}
+        />
         <DeliveryTracker />
       </div>
     </div>
